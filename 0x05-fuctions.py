3efc52272d5c4e4e1d8d1748f407708001bc801a
GabrielRodriguez12 <gabriel.rodriguezto@gmail.com>
2022-08-10 10:46:11 -0500

    Se agrego un archivo

---
+++ 0x05-fuctions.py
@@ -0,0 +1,20 @@
+def sumar(sumando1,sumando2):
+    print("Resultado de la suma:",sumando1+sumando2)
+def restar(minuendo,sustraendo):
+    print("Resultado de la resta:",minuendo-sustraendo)
+def multiplicar(multiplicando,multiplicador):
+    print("Resultado multiplicacion:",multiplicando*multiplicador)
+def potenciar(base,exponente):
+    print("Resultado potencia:",base**exponente)
+
+sumar(30,30)
+print("----------")
+restar(50,30)
+print("----------")
+multiplicar(10,10)
+print("----------")
+potenciar(5,5)
+
+
+
+
